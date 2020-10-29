7f72006155914dc2e6f0e716053474ab3a3a64f5
Nadeem Ahmad <antesoft17@outlook.com>
2020-10-29 18:20:31 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const request = require('request');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.get('/', (req, res) => {
+    console.log("REQUEST MADE");
+  request(
+    { url: 'https://www.mazda.com/' },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return res.status(500).json({ type: 'error', message: err.message });
+      }
+      res.json(body);
+    }
+  )
+});
+
+const PORT = process.env.PORT || 8003;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
