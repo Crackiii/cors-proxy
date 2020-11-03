547a7d9d255644922def2bf15d2c27e7548c8548
Nadeem Ahmad <antesoft17@outlook.com>
2020-11-03 19:45:47 +0500

    added cache

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const request = require('request');
 const https = require('https');
 const bp = require('body-parser');
+const fs = require('fs');
+const md5 = require("crypto-js/md5");
 const app = express();
 
 app.use(bp.json());
-
 app.use(express.static('public'))
-
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
@@ -21,22 +20,35 @@ app.get('/', (req, res) => {
 });
 
 app.get('/pdf-cors', (req, res) => {
-    const {file} = req.query;
+    const { file } = req.query;
+    const fileHash = md5(file).words.join("").replace(/-/g, '');
     if(!file) {
         res.send({"file-error": "no file to read"});
         return;
     }
+
+    if(fs.existsSync(`cache/${fileHash}.txt`)) {
+        const stream = fs.createReadStream(`cache/${fileHash}.txt`);
+        stream.on('open', _ => stream.pipe(res) )
+        stream.on('end', _ => {
+            res.end();
+            console.log("CACHE: File reading done !");
+        })
+        return;
+    }
+    
     https.get(file)
         .on('response', response => {
         res.setHeader('Content-Length', response.headers["content-length"]);
         //Set headers to cache the recieved data
-        res.set('Cache-Control', 'public, max-age=31557600');
+        res.set('Cache-Control', 'public, max-age=3600');
         response.on('data', data => {
+            fs.appendFileSync(`cache/${fileHash}.txt`, data, "binary",{'flags': 'a+'});
             res.write(data);
         })
         response.on('end', _ => {
             res.end();
-            console.log("File reading done !");
+            console.log("API: File reading done !");
         })
     })
 });
